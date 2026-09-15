42cb6e40898ef385dd33581c539b4fa1558a4d50
Ameen Durani <uni.ameend@gmail.com>
2026-09-15 14:05:24 +0800

    Try an even larger tile size

--- src/submission.hpp
+++ src/submission.hpp
@@ -4,7 +4,7 @@
 #include <tuple>
 #include <vector>
 
-constexpr std::size_t TILE_SIZE = 16;
+constexpr std::size_t TILE_SIZE = 128;
 
 // 2D grid of doubles stored as a single row-major buffer.
 class Grid {
